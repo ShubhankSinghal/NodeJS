1cc8a082f39f9e5e7c91209b38d1384bd8e13770
ShubhankSinghal <shubhank.singhal98@gmail.com>
2020-06-15 23:33:27 +0530

    express started

---
+++ 5/app.js
@@ -0,0 +1,23 @@
+//const http = require("http");
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+     console.log('In the middleware');
+     next();
+});
+
+app.use((req, res, next) => {
+     console.log('In the another middleware');
+     res.send('<h1>Hello from Express!</h1>');
+});
+
+/*
+const server = http.createServer(app);
+
+server.listen(3000);
+*/
+
+app.listen(3000);
