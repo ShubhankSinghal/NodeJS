f26423b5d59f804dbced32b53ea583467ced277c
ShubhankSinghal <shubhank.singhal98@gmail.com>
2020-07-01 23:24:35 +0530

    mvc covered

--- 7. The Model View Controller(MVC)/app.js
+++ 7. The Model View Controller(MVC)/app.js
@@ -8,17 +8,16 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
